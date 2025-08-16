b5d4d9956db1f5e7669033f10e13879b95a2677b
Luis Sotomayor <lsbvks77@gmail.com>
2025-08-16 15:17:38 +0200

    modify/ etiquetas y UI datos

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -48,55 +48,55 @@ const WeatherCard = () => {
   return (
     <div className="weatherCard">
       <section className="alwaysVisible">
-        <p className="info"><img src={CityIcon} alt="Ciudad" title="Ciudad"/><span>{query.city}</span></p>
-        <p className="info"><img src={SunriseIcon} alt="Salida del sol" title="Salida del sol"/><span>{sunrise} h</span></p>
-        <p className="info"><img src={LatitudeIcon} alt="Latitud" title="Latitud"/><span>{data.latitude}º</span></p>
-        <p className="info"><img src={DateIcon} alt="Fecha" title="Fecha"/><span>{date}</span></p>
-        <p className="info"><img src={SunsetIcon} alt="Puesta del sol" title="Puesta del sol"/><span>{sunset} h</span></p>
-        <p className="info"><img src={LongitudeIcon} alt="Longitud" title="Longitud"/><span>{data.longitude}º</span></p>
+        <p className="info">Ciudad <img src={CityIcon} alt="Ciudad" title="Ciudad"/><span>{query.city}</span></p>
+        <p className="info">Amanecer <img src={SunriseIcon} alt="Salida del sol" title="Salida del sol"/><span>{sunrise} h</span></p>
+        <p className="info">Latitud <img src={LatitudeIcon} alt="Latitud" title="Latitud"/><span>{data.latitude}º</span></p>
+        <p className="info"> Fecha <img src={DateIcon} alt="Fecha" title="Fecha"/><span>{date}</span></p>
+        <p className="info"> Atardecer <img src={SunsetIcon} alt="Puesta del sol" title="Puesta del sol"/><span>{sunset} h</span></p>
+        <p className="info"> Longitud <img src={LongitudeIcon} alt="Longitud" title="Longitud"/><span>{data.longitude}º</span></p>
       </section>
       {/* En la siguiente sección vamos comprobando cada uno de los parámetros. Si el usuario lo ha elegido lo ponemos en la interfaz,
       si no lo ha elegido no se incluye. */}
       <section className="visibleParams">
         {query.temperature ? (
           <>
-            <p className="info"><img src={TMaxIcon} alt="Temperatura máxima" title="Temperatura máxima"/><span>{data.daily.temperature_2m_max}ºC</span></p>
-            <p className="info"><img src={TMinIcon} alt="Temperatura mínima" title="Temperatura mínima"/><span>{data.daily.temperature_2m_min}ºC</span></p>
+            <p className="info">Tª Max <img src={TMaxIcon} alt="Temperatura máxima" title="Temperatura máxima"/><span>{data.daily.temperature_2m_max}ºC</span></p>
+            <p className="info">Tª Min <img src={TMinIcon} alt="Temperatura mínima" title="Temperatura mínima"/><span>{data.daily.temperature_2m_min}ºC</span></p>
           </>
         ) : null}
         {query.apparent_temperature ? (
           <>
             <p className="info">
-              <img src={ApTMaxIcon} alt="Sensación térmica máxima" title="Sensación térmica máxima"/><span>{data.daily.apparent_temperature_max}ºC</span>
+              Sensación Tª Max<img src={ApTMaxIcon} alt="Sensación térmica máxima" title="Sensación térmica máxima"/><span>{data.daily.apparent_temperature_max}ºC</span>
             </p>
             <p className="info">
-              <img src={ApTMinIcon} alt="Sensación térmica mínima" title="Sensación térmica mínima"/><span>{data.daily.apparent_temperature_min}ºC</span>
+              Sensación Tª Min<img src={ApTMinIcon} alt="Sensación térmica mínima" title="Sensación térmica mínima"/><span>{data.daily.apparent_temperature_min}ºC</span>
             </p>
           </>
         ) : null}
         {query.precipitation ? (
           <>
-            <p className="info"><img src={VolPrecipitacionIcon} alt="Volumen de precipitación" title="Volumen de precipitación"/><span>{data.daily.precipitation_sum} mm</span></p>
+            <p className="info">Precipitación <img src={VolPrecipitacionIcon} alt="Volumen de precipitación" title="Volumen de precipitación"/><span>{data.daily.precipitation_sum} mm</span></p>
           </>
         ) : null}
         {query.precipitation_probability ? (
           <p className="info">
-            <img src={PrecProbIcon} alt="Probabilidad de precipitación" title="Probabilidad de precipitación"/><span>{data.daily.precipitation_probability_max}%</span>
+            Prob Precipitación <img src={PrecProbIcon} alt="Probabilidad de precipitación" title="Probabilidad de precipitación"/><span>{data.daily.precipitation_probability_max}%</span>
           </p>
         ) : null}
         {query.wind_direction ? (
           <p className="info">
-            <img src={WindDirectionIcon} alt="Dirección del viento" title="Dirección del viento"/><span>{data.daily.wind_direction_10m_dominant}º</span>
+            Dirección viento <img src={WindDirectionIcon} alt="Dirección del viento" title="Dirección del viento"/><span>{data.daily.wind_direction_10m_dominant}º</span>
           </p>
         ) : null}
         {query.wind_speed ? (
-          <p className="info"><img src={WindSpeedIcon} alt="Velocidad del viento" title="Velocidad del viento"/><span>{data.daily.wind_speed_10m_max} km/h</span></p>
+          <p className="info">Velocidad viento <img src={WindSpeedIcon} alt="Velocidad del viento" title="Velocidad del viento"/><span>{data.daily.wind_speed_10m_max} km/h</span></p>
         ) : null}
         {query.humidity ? (
-          <p className="info"><img src={HumidityIcon} alt="Humedad relativa media" title="Humedad relativa media"/><span>{Math.round(mean(data.hourly.relative_humidity_2m)*10)/10}%</span></p>
+          <p className="info">Humedad <img src={HumidityIcon} alt="Humedad relativa media" title="Humedad relativa media"/><span>{Math.round(mean(data.hourly.relative_humidity_2m)*10)/10}%</span></p>
         ) : null}
         {query.surface_pressure ? (
-          <p className="info"><img src={PressureIcon} alt="Presión atmosférica media" title="Presión atmosférica media"/><span>{Math.round(mean(data.hourly.surface_pressure)*10)/10} hPa</span></p>
+          <p className="info">Presión <img src={PressureIcon} alt="Presión atmosférica media" title="Presión atmosférica media"/><span>{Math.round(mean(data.hourly.surface_pressure)*10)/10} hPa</span></p>
         ) : null}
       </section>
     </div>
